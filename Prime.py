e39215ea32e01302beef8fa76baf3f987ce376f7
Arsalan Ali <arsalan34568@gmail.com>
2025-08-19 19:34:13 +0500

    Add files via upload

---
+++ Prime.py
@@ -0,0 +1,12 @@
+def is_prime(num):
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                print("Not Prime")
+                break
+        else:
+            print("Prime")
+    else:
+        print("Not Prime")
+
+is_prime(11)
